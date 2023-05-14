9b2f6aa10a5cd9cec7a332e8934835c0c4e5e133
L.A.A <mr.leoalves@gmail.com>
2023-05-14 19:06:41 -0300

    Finalização da tela de menu e adição de eventos aos botoes de cadastrar

--- src/View/Gui/Menu.java
+++ src/View/Gui/Menu.java
@@ -1,5 +1,10 @@
 package View.Gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -9,12 +14,16 @@ import javax.swing.JFrame;
  */
 public class Menu extends JFrame{
     
-    private final JButton btn_cadHeroi,btn_cadVilao,btn_cadUsr, btn_mostrarHero,btn_mostraVilao,btn_mostrarUsr;
+    private final JButton btn_cadHeroi,btn_cadVilao,btn_cadUsr;
+    private final JButton btn_mostrarHero,btn_mostraVilao,btn_mostrarUsr;
+    private final JButton btn_alteraHero,btn_alteraVilao,btn_alteraUsr;
+    private final JButton btn_deletaHero,btn_deletaVilao,btn_deletaUsr;
     
     public Menu(){
-        setSize(600, 400);
+        setSize(500, 400);
         setTitle("Menu");
         setLayout(null);
+        setResizable(false);
         
         btn_cadHeroi    = new  JButton("Cadastrar heroi");
         btn_cadVilao    = new  JButton("Cadastrar Vilão");
@@ -24,6 +33,14 @@ public class Menu extends JFrame{
         btn_mostraVilao = new  JButton("Mostrar vilões");
         btn_mostrarUsr  = new  JButton("Mostrar usuários");
         
+        btn_alteraHero = new JButton("Atualizar heroi");
+        btn_alteraVilao = new JButton("Atualizar vilão");
+        btn_alteraUsr = new JButton("Atualizar usuário");
+        
+        btn_deletaHero = new JButton("Deletar Heroi");
+        btn_deletaVilao = new JButton("Deletar Vilão");
+        btn_deletaUsr = new JButton("Deletar usuário");
+        
         btn_cadHeroi.setBounds(20, 20, 150, 30);
         btn_cadVilao.setBounds(20, 60, 150, 30);
         btn_cadUsr.setBounds(20, 100, 150, 30);
@@ -31,6 +48,37 @@ public class Menu extends JFrame{
         btn_mostrarHero.setBounds(20, 140, 150, 30);
         btn_mostraVilao.setBounds(20, 180, 150, 30);
         btn_mostrarUsr.setBounds(20,220 , 150, 30);
+        
+        btn_alteraHero.setBounds(200, 20, 150, 30);
+        btn_alteraVilao.setBounds(200, 60, 150, 30);
+        btn_alteraUsr.setBounds(200, 100, 150, 30);
+        
+        btn_deletaHero.setBounds(200, 140, 150, 30);
+        btn_deletaVilao.setBounds(200, 180, 150, 30);
+        btn_deletaUsr.setBounds(200,220 , 150, 30);
+        
+        btn_cadHeroi.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    cadHeroi();
+                } catch (ParseException ex) {
+                    System.out.println("Ocorreu um erro ao executar a ação :" + ex);
+                }
+//                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+        
+        btn_cadVilao.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    CadVilao();
+                } catch (ParseException ex) {
+                    System.out.println("Ocorreu um erro ao executar a ação :" + ex);
+                }
+            }
+        });
         
         add(btn_cadHeroi);
         add(btn_cadVilao);
@@ -40,7 +88,26 @@ public class Menu extends JFrame{
         add(btn_mostraVilao);
         add(btn_mostrarUsr);
         
+        add(btn_alteraHero);
+        add(btn_alteraVilao);
+        add(btn_alteraUsr);
+        
+        add(btn_deletaHero);
+        add(btn_deletaVilao);
+        add(btn_deletaUsr);
+        
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
+    
+    private void cadHeroi() throws ParseException{
+            this.dispose();
+            CadHeroi cadHeroi = new CadHeroi();
+    }
+    
+    private void CadVilao() throws ParseException{
+        this.dispose();
+        CadVilao cadVilao = new CadVilao();
+    }
+    
 }
--- src/cad_heroiseviloes/Main.java
+++ src/cad_heroiseviloes/Main.java
@@ -1,11 +1,6 @@
 package cad_heroiseviloes;
 
-import View.Gui.CadHeroi;
 import View.Gui.Menu;
-import View.Gui.TelaLogin;
-import java.text.ParseException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -17,13 +12,13 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try {
-            CadHeroi cad = new CadHeroi();
-            //TelaLogin log = new TelaLogin();
-             new Menu();
-        } catch (ParseException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        //try {
+        // CadHeroi cad = new CadHeroi();
+        //TelaLogin log = new TelaLogin();
+        Menu menu = new Menu();
+        //} catch (ParseException ex) {
+        //    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        //}
     }
     
 }
