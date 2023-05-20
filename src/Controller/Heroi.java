0cc76fc4fa1f339daa1a2905a7da6818b46a4d5b
L.A.A <mr.leoalves@gmail.com>
2023-05-19 22:36:37 -0300

    Insert da classe heroi funcionando e com preparedStatement

--- src/Controller/Heroi.java
+++ src/Controller/Heroi.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controller;
 
 import Controller.Template.BDObject;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 import java.sql.SQLException;
-import java.sql.Statement;
 
 /**
  *
@@ -16,46 +12,36 @@ import java.sql.Statement;
  */
 public class Heroi extends BDObject {
 
-    /* 
-    String nome,identidade,inimigo;
-     int idade;
-     double altura;
-     */
     private static String urlJDBC = "jdbc:mysql://localhost:3306/heroiseviloes";
     private static String usuario = "root";
     private static String senha = "751204Laa.15";
 
+    ConnectionSingleton inst = ConnectionSingleton.getInstancy();
+    Connection con = inst.getConexao();
+
     public Heroi(String nome, String identidade, int idade, double altura, String inimigo) {
 
     }
-        
-        ConnectionSingleton singleton  = ConnectionSingleton.getInstancy();
-       
+
     public void inserir(String nome_heroi, String identidade_heroi, int idade_heroi, double altura_heroi, String inimigo_heroi) {
 
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            String query = "INSERT INTO HEROI VALUES (?,?,?,?,?)";
+            PreparedStatement pst = con.prepareStatement(query);
 
-            java.sql.Connection conexao = DriverManager.getConnection(urlJDBC, usuario, senha);
+            pst.setString(1, nome_heroi);
+            pst.setString(2, identidade_heroi);
+            pst.setInt(3, idade_heroi);
+            pst.setDouble(4, altura_heroi);
+            pst.setString(5, inimigo_heroi);
 
-            if (conexao != null) {
-                System.out.println("Banco acessado com sucesso");
-                String cmd = "INSERT INTO HEROI (nome_heroi, identidade_heroi, idade_heroi,altura_heroi, inimigo_heroi) VALUES"
-                        + "('" + nome_heroi + "','" + identidade_heroi + "'," + idade_heroi + "," + altura_heroi + ",'" + inimigo_heroi + "')";
-                System.out.println(cmd);
-                Statement inserir = (Statement) conexao.createStatement();
-                inserir.execute(cmd);
-                conexao.close();
-            } else {
-                System.out.println("Não foi possivel acessar o banco de dados");
-            }
+            pst.executeUpdate();
 
-        } catch (SQLException exsql) {
-            System.out.println("Ocorreu um erro ao acessar o banco de dados :" + exsql);
-        } catch (ClassNotFoundException exClasse) {
-            System.err.println("Ocorreu um erro ao tentar carregar a clasee :" + exClasse);
+            System.out.println("Fino do fino");
+
+        } catch (SQLException ex) {
+            System.out.println("Ocorreu um erro ao inserir no banco de dados :" + ex);
         }
-
     }
 
     public void atualizar() {
