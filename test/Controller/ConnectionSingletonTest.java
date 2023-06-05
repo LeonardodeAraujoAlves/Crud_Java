5fa27d3e8797ab5d2b23f4c7e71cddf1eb136aa3
L.A.A <mr.leoalves@gmail.com>
2023-06-05 13:33:13 -0300

    Criado os testes para a conexão singleton , testes iniciais com 50 por cento de aprovação, o ultimo deu 100% de aprovaçaõ

---
+++ test/Controller/ConnectionSingletonTest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import java.sql.Connection;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author L.A.A
+ */
+public class ConnectionSingletonTest {
+    /**
+     * Test of getInstance method, of class ConnectionSingleton.
+     */
+    @Test
+    public void testGetInstance() {
+        System.out.println("getInstance");
+        ConnectionSingleton expResult = ConnectionSingleton.getInstance();
+        ConnectionSingleton result = ConnectionSingleton.getInstance();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        assertSame(expResult, result);
+    }
+
+    /**
+     * Test of getConexao method, of class ConnectionSingleton.
+     */
+    @Test
+    public void testGetConexao() {
+        System.out.println("getConexao");
+        ConnectionSingleton instance = ConnectionSingleton.getInstance();
+        Connection con = instance.getConexao();
+        assertNull(con);
+    }
+}
